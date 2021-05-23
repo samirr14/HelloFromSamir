562078815993504d54bd953f33bb211da142744a
Samir Rodriguez <samirrodriguez14@gmail.com>
2021-05-23 00:08:14 -0400

    Change 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello my name is Samir");
         System.out.println("Hi, Jose had a problem with his Oracle jdk and couldn't commit");
+        System.out.println("Hello, my name is Jose");
     }
 }
